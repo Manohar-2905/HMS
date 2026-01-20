80438cf4768f0cded9f6e41050befc03dfef2ff9
manohar-2905 <kumarmanohar6206@gmail.com>
2026-01-20 14:59:33 +0530

    Update ADMIN_EMAIL for production

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -38,7 +38,7 @@ const sendEmail = async (options) => {
             service: 'gmail',
             auth: {
                 type: 'OAuth2',
-                user: process.env.EMAIL_USER,
+                user: process.env.GMAIL_USER || process.env.EMAIL_USER,
                 clientId: process.env.GMAIL_CLIENT_ID,
                 clientSecret: process.env.GMAIL_CLIENT_SECRET,
                 refreshToken: process.env.GMAIL_REFRESH_TOKEN
