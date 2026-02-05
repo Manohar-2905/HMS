48805cc47e8898d3558cb1b7fd3436dc3eaeeb69
Manohar-2905 <kumarmanohar6206@gmail.com>
2026-02-05 14:06:52 +0530

    Delete deployment directory

--- deployment/routes/invoiceRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getInvoice } = require('../controllers/invoiceController');
-const { protect, admin } = require('../middleware/authMiddleware');
-
-router.get('/:userId', protect, admin, getInvoice);
-
-module.exports = router;
